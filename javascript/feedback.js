d968ef311b2473d72ec26d0698666e00121d0493
abiucom <adzalulliyah@gmail.com>
2023-10-08 22:26:43 +0700

    feedback form

---
+++ javascript/feedback.js
@@ -0,0 +1,44 @@
+const API_BASE_URL = "https://localhost:3000";
+
+async function fetchMessages() {
+  try {
+    const response = await fetch(`${API_BASE_URL}/messages`);
+    const data = await response.json();
+    const messageContainer = document.getElementById("message-container");
+    messageContainer.innerHTML = "";
+    data.data.forEach((message) => {
+      const newMessageList = document.createElement("li");
+      newMessageList.textContent = `${message.name}: ${message.message}`;
+      messageContainer.appendChild(newMessageList);
+    })
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function sendMessage() {
+  const nama = document.getElementById("input-nama").value;
+  const email = document.getElementById("input-email").value;
+  const massage = document.getElementById("input-massage").value;
+
+  try {
+    const sendMessage = await fetch(`${API_BASE_URL}/messages`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name, message })
+  })
+  const response = await sendMessage.json();
+
+  if (response.success) {
+    await fetchMessages(); // Refresh the message list after adding a new message
+  } else {
+    console.error(response.message);
+  }
+  } catch (error) {
+    console.error('Error adding message:', error);
+  }
+}
+
+fetchMessages()
