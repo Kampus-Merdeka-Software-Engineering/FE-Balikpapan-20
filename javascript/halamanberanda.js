197119a913f076d08b141017c47d03879539c590
FauzanHasibuan78 <fauzanhasibuan78@gmail.com>
2023-10-10 13:44:23 +0700

    FE fix

--- javascript/halamanberanda.js
+++ javascript/halamanberanda.js
@@ -1,48 +1,69 @@
 const API_BASE_URL = "https://kind-tan-spider-slip.cyclic.app";
-async function addAllTracking() {
-  const nomorResi = document.getElementById("nomor-resi").value;
+
+async function lacak() {
+  const nomerResi = document.getElementById("nomor-resi").value;
 
   try {
-    const response = await fetch(`${API_BASE_URL}/tracking`, {
-      method: "GET",
-      body: JSON.stringify({ nomorResi }),
-    });
+    const response = await fetch(`${API_BASE_URL}/lacak`)
     const data = await response.json();
-
-    // Menampilkan hasil pelacakan di div output-box1
-    const outputBox1 = document.getElementById("output-box1");
-    outputBox1.innerHTML = `
-        Nomor Resi: ${nomorResi}<br>
-        Nama Penerima: ${data.receiverName}<br>
-        Nomor HP: ${data.receiverPhoneNumber}<br>
-        Status Pengiriman: ${data.status}
-      ${JSON.stringify(data)}`;
-  } catch (error) {
-    console.error("Tidak dapat menemukan nomor resi Anda: ", error);
-  }
-}
-
-async function addAllCheck() {
-  const origin = document.getElementById("origin").value;
-
-  try {
-    const response = await fetch(`${API_BASE_URL}/tracking`, {
-      method: "GET",
-      body: JSON.stringify({ origin }),
-    });
-    const data = await response.json();
+    console.log(data)
 
     // Menampilkan hasil pelacakan di div output-box2
-    const outputBox2 = document.getElementById("output-box2");
+    const outputBox2 = document.getElementById("output-box1");
+    if (!Array.isArray(data)) {
+      console.log("masuk")
+      outputBox2.innerHTML = 
+     "<h3>barang tidak di temukan</h3>"
+      return;
+    }
+    if (data.length > 0 ) {
+      const result = data.filter(resi => resi.id == nomerResi)
+      if (result.length === 0) {
+      outputBox2.innerHTML = 
+     "<h3>barang tidak di temukan</h3>"
+      return;
+      }
+      outputBox2.innerHTML = ` 
+      id: ${result[0].id}<br>
+      nama: ${result[0].nama}<br>
+      numberTelpon: ${result[0].numberTelpon}<br>
+      statusPengiriman: ${result[0].statusPengiriman}`;
+      return;
+    }
+    
     outputBox2.innerHTML = `
       origin: ${origin}<br>
       destination: ${destination}<br>
       weight: ${weight}<br>
-      price: ${price}
-    ${JSON.stringify(data)}`;
+      price: ${data.tarif}`;
   } catch (error) {
     console.error("Tidak dapat menemukan destinasi Anda: ", error);
-  } finally {
-    fetchCheck();
+  }
+}
+
+async function checkTarif() {
+  const origin = document.getElementById("origin").value;
+  const destination = document.getElementById("destination").value;
+  const weight = document.getElementById("weight").value;
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/tarif?origin=${origin}&destination=${destination}&weight=${weight}`)
+    const data = await response.json();
+    console.log(data)
+
+    // Menampilkan hasil pelacakan di div output-box2
+    const outputBox2 = document.getElementById("output-box2");
+    if (data.tarif === null) {
+      outputBox2.innerHTML = '<h3>tarif tidak tersedia</h3>'
+      return;
+    }
+    
+    outputBox2.innerHTML = `
+      origin: ${origin}<br>
+      destination: ${destination}<br>
+      weight: ${weight}<br>
+      price: ${data.tarif}`;
+  } catch (error) {
+    console.error("Tidak dapat menemukan destinasi Anda: ", error);
   }
 }
